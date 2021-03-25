dd75373b15ab5559894c2ff0629de2f961a6265a
Md Inzamam Ul Haque (64f) <haquem@ornl.gov>
2021-03-25 14:23:10 -0400

    Updating test_image.py

--- test_image.py
+++ test_image.py
@@ -1,14 +1,34 @@
 import torch
+from torch import nn
 from torchvision.models import densenet
+import torch.distributed as dist
 import train_densenet
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
 from train_densenet import cxr_densenet
 import torchvision.transforms as transforms
 from PIL import Image
+import os
+
+model = cxr_densenet('densenet121', pretrained=True)
+rank=0
+world_size=1
+local_rank=0
+
+#gpunum = local_rank 
+#device = torch.device('cuda', gpunum)
+#model = model.to(device)
+
+
+dist.get_world_size()
+dist.init_process_group('nccl')
+#model = DDP(model,device_ids=[gpunum],output_device=gpunum)
+
 
 normalize = transforms.Normalize(mean=[0.449], #[0.485, 0.456, 0.406],
                                  std=[0.226]) #[0.229, 0.224, 0.225]),
 
-model = cxr_densenet('densenet121', pretrained=True)
 model.load_state_dict(torch.load('/home/64f/cxr/cxr_classification/out2048x2048/model_epoch4.pt'))
 model.eval()
 
