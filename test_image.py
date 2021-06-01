5e31566fcbce643f3bbe8d6d0f8fdea4970711e4
Md Inzamam Ul Haque (64f) <haquem@ornl.gov>
2021-06-01 11:43:02 -0400

    Update to compute probabilities

--- test_image.py
+++ test_image.py
@@ -9,16 +9,17 @@ normalize = transforms.Normalize(mean=[0.449], #[0.485, 0.456, 0.406],
                                  std=[0.226]) #[0.229, 0.224, 0.225]),
 
 model = cxr_net('densenet121', pretrained=True)
-model.load_state_dict(torch.load('/home/64f/cxr/cxr_classification/out256x256/model_epoch24.pt'))
+model.load_state_dict(torch.load('/home/64f/cxr/cxr_classification/saved_models/256/model_epoch11.pt'))
 model.eval()
 
 transform = transforms.Compose([transforms.ToTensor(),normalize])
 
-img = Image.open('/home/64f/cxr/cxr_classification/36c192b5-e5f11e39-3d2677d2-18ee255c-9958a698.jpg')  # Load image as PIL.Image
+img = Image.open('/home/64f/cxr/cxr_classification/53bdbaea-2fef8033-aca685ff-2924187a-b092c1cd.jpg')  # Load image as PIL.Image
 x = transform(img)  # Preprocess image
 x = x.unsqueeze(0)  # Add batch dimension
 
 output = model(x)  # Forward pass
-pred = torch.argmax(output, 1)  # Get predicted class if multi-class classification
-print('Image predicted as ', pred)
-
+print(output)
+sm = torch.nn.Softmax(dim=1) #softmax function for getting probabilities
+pred = sm(output)  # Get predictions
+print(pred)
