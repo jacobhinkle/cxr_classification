7755e95c445f426ea0f6df4252b513993dc2af85
Jacob Hinkle <hinklejd@ornl.gov>
2021-06-25 08:13:13 -0400

    Get ensemble_weighted.py to train.
    It now trains, but we prob don't need to do SGD and it may hurt us.
    Still, the weights we find seem pretty believable (you may want to try
    decreasing the LR and running longer). @64f you will definitely want to
    plot the loss which should decrease very slightly over time and
    converge. I saw very slightly since we don't expect a massive
    improvement due to stacked ensembling over soft voting (unweighted).

--- ensemble_weighted.py
+++ ensemble_weighted.py
@@ -2,7 +2,9 @@ import numpy as np
 import torch
 from torch import dstack
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader, TensorDataset
+from tqdm import tqdm
 
 def stacked_dataset(members):
     stackX = None
@@ -66,35 +68,31 @@ class SoftmaxWeightedEnsembleBinary(nn.Module):
     def forward(self, *xs):
         xs = torch.stack(xs, dim=2)
         # logsoftmax = x - LSE(x)
-        logw = torch.logsoftmax(self.weight_l, dim=1)
-        logp = torch.logsigmoid(xs)
+        logw = F.log_softmax(self.weight_l, dim=1)
+        logp = F.logsigmoid(xs)
         # log probability of ensemble prediction
         # Note xs is 3D with dimensions [batch, label, scale], while logw is
         # [label, scale], so we unsqueeze a new dimension before adding
         logwp = logw[None, :, :] + logp  # equiv to log(w * p)
+        # Ensemble probability is sumexp of logwp along scale dimension. Get log of that
+        logp_ens = torch.logsumexp(logwp, dim=-1)
         # transform to logit instead of logprob
-        # this is logit(p)=log(p/(1-p)) for p=exp(logwp)
-        logitwp = - torch.softplusinv(- logwp)
-        return logitwp
+        # this is logit(p)=log(p/(1-p)) for p=exp(logp_ens)
+        # PyTorch as of 1.9 doesn't have a softplusinv function, so we make one
+        #logitp_ens = - torch.softplusinv(- logp_ens)
+        logitp_ens = logp_ens - torch.log(- torch.expm1(logp_ens))
+        return logitp_ens
 
 if __name__ == '__main__':
-    logits_256 = torch.load('logits_256.pt').cpu()
-    logits_512 = torch.load('logits_512.pt').cpu()
-    logits_1024 = torch.load('logits_1024.pt').cpu()
-    logits_2048 = torch.load('logits_2048.pt').cpu()
+    logits_256 = torch.load('logits_256.pt', map_location='cpu')
+    logits_512 = torch.load('logits_512.pt', map_location='cpu')
+    logits_1024 = torch.load('logits_1024.pt', map_location='cpu')
+    logits_2048 = torch.load('logits_2048.pt', map_location='cpu')
 
-    testY_256 = torch.load('testY_256.pt').cpu()
-
-
-    print(logits_256.shape, testY_256.shape)
+    trainY = torch.load('testY_256.pt', map_location='cpu')
 
     logits = [logits_256, logits_512, logits_1024, logits_2048]
 
-    trainX = stacked_dataset(logits)
-    trainY = testY_256
-    print(trainX.shape)
-
-    #dataset = trainData(trainX, trainY)
     dataset = TensorDataset(*logits, trainY)
     dataloader = DataLoader(dataset, batch_size=512, shuffle=True, num_workers=8)
 
@@ -106,31 +104,18 @@ if __name__ == '__main__':
 
     criterion = nn.BCEWithLogitsLoss()
 
-    learning_rate = 0.001
+    learning_rate = 1e1
 
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-    for epoch in range(100):
-        running_loss = 0.0
-        for i, batch in enumerate(dataloader):
+    for epoch in tqdm(range(100), desc='epoch'):
+        print("Current weights", torch.softmax(model.weight_l, dim=-1))
+        itbar = tqdm(enumerate(dataloader), desc='iter')
+        for i, batch in itbar:
             labels = batch[-1]
             optimizer.zero_grad()
             outputs = model(*batch[:-1])
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-            if i % 500 == 499:    # print every 500 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 500))
-                running_loss = 0.0
-
-
-
-
-
-
-
-
+            itbar.set_postfix(loss=loss.item())
