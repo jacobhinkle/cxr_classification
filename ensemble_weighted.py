883ca59829a7ae95b521bfa176aad74f3632efd4
Md Inzamam Ul Haque (64f) <haquem@ornl.gov>
2021-06-24 23:30:17 -0400

    Clean the code

--- ensemble_weighted.py
+++ ensemble_weighted.py
@@ -2,41 +2,48 @@ import numpy as np
 import torch
 from torch import dstack
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, TensorDataset
 
-def stacked_dataset(members):
-    stackX = None
-    for logit in members:
-        # stack predictions into [rows, members, probabilities]
-        if stackX is None:
-            stackX = logit
-        else:
-            stackX = dstack((stackX, logit))
-    # flatten predictions to [rows, members x probabilities]
-    stackX = stackX.reshape((stackX.shape[0], stackX.shape[1]*stackX.shape[2]))
-    return stackX
+class SoftmaxWeightedEnsembleBinary(nn.Module):
+    """
+    Multi-label ensemble averaging of sigmoid logits.
 
-class LogisticRegressionModel(nn.Module):
-    def __init__(self, input_size, num_classes):
-        super(LogisticRegressionModel, self).__init__()
-        self.linear = nn.Linear(input_dim, output_dim)
+    This model has a single parameter w which is a 2D tensor such that the
+    weight for finding f and scale s is w[f, s]. This parametrization satisfies
+    sum_s w[f, s] = 1.
 
-    def forward(self, x):
-        out = self.linear(x)
-        return out
+    Given logits l[f, s], this provides a numerically stable way to compute
+    logit(sum_s w[f, s] sigmoid(l[f, s])) for each finding f.
 
-class trainData(Dataset):
-    def __init__(self, logits, labels):
-        self.samples = logits
-        self.labels = labels
+    Note that each label f is treated separately, even though everything is
+    computed at once for the whole batch.
 
-    def __len__(self):
-        return int(self.samples.shape[0])
+    When trained, torch.softmax(self.weight_l) gives the weights used to perform
+    ensembling, and is interpreted as the importance of each scale, for
+    predicting a given finding.
 
-    def __getitem__(self, idx):
-        X = self.samples[idx,:]
-        y = self.labels[idx,:]
-        return X,y
+    Note that the weights are initialized to uniform.
+    """
+    def __init__(self, num_labels, num_learners):
+        super().__init__()
+        # logit parameterization of weights
+        self.weight_l = nn.Parameter(torch.zeros(num_labels, num_learners))
+
+    def forward(self, *xs):
+        xs = torch.stack(xs, dim=2)
+        # logsoftmax = x - LSE(x)
+        logsoftmax = nn.LogSoftmax(dim=1)
+        logsigmoid = nn.LogSigmoid()
+        logw = logsoftmax(self.weight_l)
+        logp = logsigmoid(xs)
+        # log probability of ensemble prediction
+        # Note xs is 3D with dimensions [batch, label, scale], while logw is
+        # [label, scale], so we unsqueeze a new dimension before adding
+        logwp = logw[None, :, :] + logp  # equiv to log(w * p)
+        # transform to logit instead of logprob
+        # this is logit(p)=log(p/(1-p)) for p=exp(logwp)
+        logitwp = - torch.softplusinv(- logwp)
+        return logitwp
 
 if __name__ == '__main__':
     logits_256 = torch.load('logits_256.pt').cpu()
@@ -51,19 +58,19 @@ if __name__ == '__main__':
 
     logits = [logits_256, logits_512, logits_1024, logits_2048]
 
-    trainX = stacked_dataset(logits)
     trainY = testY_256
-    print(trainX.shape)
 
-    dataset = trainData(trainX, trainY)
+    #dataset = trainData(trainX, trainY)
+    dataset = TensorDataset(*logits, trainY)
     dataloader = DataLoader(dataset, batch_size=512, shuffle=True, num_workers=8)
 
-    input_dim = 56
-    output_dim = 14
+    num_labels = 14
+    num_learners = len(logits)
 
-    model = LogisticRegressionModel(input_dim, output_dim)
+    #model = LogisticRegressionModel(num_labels * num_learners, num_labels)
+    model = SoftmaxWeightedEnsembleBinary(num_labels, num_learners)
 
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.BCEWithLogitsLoss()
 
     learning_rate = 0.001
 
@@ -71,13 +78,10 @@ if __name__ == '__main__':
 
     for epoch in range(100):
         running_loss = 0.0
-        for i, (data, labels) in enumerate(dataloader):
-            # Load images as Variable
-            data = data.requires_grad_()
-            labels = labels
-
+        for i, batch in enumerate(dataloader):
+            labels = batch[-1]
             optimizer.zero_grad()
-            outputs = model(data)
+            outputs = model(*batch[:-1])
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
@@ -88,11 +92,4 @@ if __name__ == '__main__':
                 print('[%d, %5d] loss: %.3f' %
                       (epoch + 1, i + 1, running_loss / 500))
                 running_loss = 0.0
-
-
-
-
-
-
-
 
