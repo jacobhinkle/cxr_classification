f21ecace388cb005b9f3da2bfa41e1ac8f4549ec
Jacob Hinkle <hinklejd@ornl.gov>
2021-06-24 21:49:55 -0400

    Sketch out simple stacking binary ensemble model.
    I haven't tested it yet! See docstring.

--- ensemble_weighted.py
+++ ensemble_weighted.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 from torch import dstack
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, TensorDataset
 
 def stacked_dataset(members):
     stackX = None
@@ -38,6 +38,45 @@ class trainData(Dataset):
         y = self.labels[idx,:]
         return X,y
 
+class SoftmaxWeightedEnsembleBinary(nn.Module):
+    """
+    Multi-label ensemble averaging of sigmoid logits.
+
+    This model has a single parameter w which is a 2D tensor such that the
+    weight for finding f and scale s is w[f, s]. This parametrization satisfies
+    sum_s w[f, s] = 1.
+
+    Given logits l[f, s], this provides a numerically stable way to compute
+    logit(sum_s w[f, s] sigmoid(l[f, s])) for each finding f.
+
+    Note that each label f is treated separately, even though everything is
+    computed at once for the whole batch.
+
+    When trained, torch.softmax(self.weight_l) gives the weights used to perform
+    ensembling, and is interpreted as the importance of each scale, for
+    predicting a given finding.
+
+    Note that the weights are initialized to uniform.
+    """
+    def __init__(self, num_labels, num_learners):
+        super().__init__()
+        # logit parameterization of weights
+        self.weight_l = nn.Parameter(torch.zeros(num_labels, num_learners))
+
+    def forward(self, *xs):
+        xs = torch.stack(xs, dim=2)
+        # logsoftmax = x - LSE(x)
+        logw = torch.logsoftmax(self.weight_l, dim=1)
+        logp = torch.logsigmoid(xs)
+        # log probability of ensemble prediction
+        # Note xs is 3D with dimensions [batch, label, scale], while logw is
+        # [label, scale], so we unsqueeze a new dimension before adding
+        logwp = logw[None, :, :] + logp  # equiv to log(w * p)
+        # transform to logit instead of logprob
+        # this is logit(p)=log(p/(1-p)) for p=exp(logwp)
+        logitwp = - torch.softplusinv(- logwp)
+        return logitwp
+
 if __name__ == '__main__':
     logits_256 = torch.load('logits_256.pt').cpu()
     logits_512 = torch.load('logits_512.pt').cpu()
@@ -55,15 +94,17 @@ if __name__ == '__main__':
     trainY = testY_256
     print(trainX.shape)
 
-    dataset = trainData(trainX, trainY)
+    #dataset = trainData(trainX, trainY)
+    dataset = TensorDataset(*logits, trainY)
     dataloader = DataLoader(dataset, batch_size=512, shuffle=True, num_workers=8)
 
-    input_dim = 56
-    output_dim = 14
+    num_labels = 14
+    num_learners = len(logits)
 
-    model = LogisticRegressionModel(input_dim, output_dim)
+    #model = LogisticRegressionModel(num_labels * num_learners, num_labels)
+    model = SoftmaxWeightedEnsembleBinary(num_labels, num_learners)
 
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.BCEWithLogitsLoss()
 
     learning_rate = 0.001
 
@@ -71,13 +112,10 @@ if __name__ == '__main__':
 
     for epoch in range(100):
         running_loss = 0.0
-        for i, (data, labels) in enumerate(dataloader):
-            # Load images as Variable
-            data = data.requires_grad_()
-            labels = labels
-
+        for i, batch in enumerate(dataloader):
+            labels = batch[-1]
             optimizer.zero_grad()
-            outputs = model(data)
+            outputs = model(*batch[:-1])
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
