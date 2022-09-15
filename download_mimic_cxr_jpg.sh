e2c9d23142bc6ac4289dd6f9ba947554a0947790
Jacob Hinkle <hinklejd@ornl.gov>
2022-09-15 09:14:59 -0400

    Accept destination argument in download script

--- download_mimic_cxr_jpg.sh
+++ download_mimic_cxr_jpg.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+
+if [ $# -ne 1 ]
+then
+    echo "Please provide a destination path for the download"
+    exit 1
+fi
+
+DEST=$1
+
+mkdir -p $DEST
+
 # Install gsutil following Google's instructions then run gcloud init first
 gsutil -m cp -r \
   "gs://mimic-cxr-jpg-2.0.0.physionet.org/LICENSE.txt" \
@@ -10,4 +21,5 @@ gsutil -m cp -r \
   "gs://mimic-cxr-jpg-2.0.0.physionet.org/mimic-cxr-2.0.0-negbio.csv.gz" \
   "gs://mimic-cxr-jpg-2.0.0.physionet.org/mimic-cxr-2.0.0-split.csv.gz" \
   "gs://mimic-cxr-jpg-2.0.0.physionet.org/mimic-cxr-2.0.0-reports.csv.gz" \
-  .
+  "gs://mimic-cxr-jpg-2.0.0.physionet.org/split-paths.csv.gz" \
+  $DEST
